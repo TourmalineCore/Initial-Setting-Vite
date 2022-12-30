16806edc7dfcdfed225d83ba60f38d0e3ef8c86f
Yasnovsky <andrey5.1997@mail.ru>
2022-12-30 15:02:32 +0500

    refactor: add folder for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 
 export function App() {
   return (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
+import reactLogo from '../../assets/react.svg';
 import './Home.scss';
 
 function Home() {
