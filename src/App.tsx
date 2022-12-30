b61851963e01410825f3c28a4e479eb49e35a83e
Yasnovsky <andrey5.1997@mail.ru>
2022-12-30 14:06:10 +0500

    refactor(app): removed an extra component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,10 @@ import Home from './pages/Home';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/*" element={<Home />} />
-    </Routes>
-  );
-}
-
-export function WrappedApp() {
-  return (
     <HashRouter>
-      <App />
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
     </HashRouter>
   );
 }
